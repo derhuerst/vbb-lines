1016bd67cae4aedea8db69ea00a5e8e0788f84fa
Jannis R <mail@jannisr.de>
2020-04-18 16:40:27 +0200

    download script: cache using ETags
    [ci skip]

--- download.sh
+++ download.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-curl -L --compressed https://vbb-gtfs.jannisr.de/latest/routes.csv > routes.csv
-curl -L --compressed https://vbb-gtfs.jannisr.de/latest/trips.csv > trips.csv
-curl -L --compressed https://vbb-gtfs.jannisr.de/latest/stop_times.csv > stop_times.csv
+set -e
+
+base_url='https://vbb-gtfs.jannisr.de/latest/'
+download () {
+	curl -L --compressed --etag-compare "$1.etag" --etag-save "$1.etag" $base_url$1 -o $1
+}
+
+download 'routes.csv'
+download 'trips.csv'
+download 'stop_times.csv'
+
+ls -lh *.csv
